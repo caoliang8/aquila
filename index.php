18808d08b619c5f32a309c7514b8fe90cd8aa8dd
caoliang8 <849289938@qq.com>
2024-11-13 19:58:48 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html  <?php language_attributes();?>>
+<head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
